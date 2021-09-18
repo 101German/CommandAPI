c15fac7fbd13fdb1b4f760aa3e214b63e5621f8b
German <geras210e@gmail.com>
2021-09-18 21:38:03 +0300

    Modify the ConfigureServices method

--- CommandAPI/Startup.cs
+++ CommandAPI/Startup.cs
@@ -2,6 +2,8 @@ using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -15,8 +17,16 @@ namespace CommandAPI
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
             services.AddControllers();
             services.AddScoped<ICommandRepo, MockCommandRepo>();
         }
