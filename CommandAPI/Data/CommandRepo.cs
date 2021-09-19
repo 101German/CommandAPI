78940c370d6c59a71f89215afa3c2a0af16d3443
German <geras210e@gmail.com>
2021-09-19 12:21:57 +0300

    Add CommandRepo

---
+++ CommandAPI/Data/CommandRepo.cs
@@ -0,0 +1,50 @@
+﻿using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly CommandContext _repositoryContext;
+
+        public CommandRepo(CommandContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _repositoryContext.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _repositoryContext.CommandItems.FirstOrDefault(cmd => cmd.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
