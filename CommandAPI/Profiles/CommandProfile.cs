1518de4460dc1af691604acabfe3772776b6390f
German <geras210e@gmail.com>
2021-09-19 18:34:25 +0300

    Add CommandProfile

---
+++ CommandAPI/Profiles/CommandProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CommandAPI.DTO;
+using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Profiles
+{
+    public class CommandProfile:Profile
+    {
+        public CommandProfile()
+        {
+            CreateMap<Command, CommandReadDTO>();
+        }
+    }
+}
