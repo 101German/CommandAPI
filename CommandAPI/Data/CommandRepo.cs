76d28a8b1714bba3631db9ec5660a842c39b747d
German <geras210e@gmail.com>
2021-09-20 17:23:49 +0300

    Add tests for CommandsController

--- CommandAPI/Data/CommandRepo.cs
+++ CommandAPI/Data/CommandRepo.cs
@@ -1,10 +1,8 @@
 ﻿using CommandAPI.Models;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CommandAPI.Data
 {
--- CommandAPI/Profiles/CommandProfile.cs
+++ CommandAPI/Profiles/CommandProfile.cs
@@ -1,11 +1,7 @@
 ﻿using AutoMapper;
 using CommandAPI.DTO;
 using CommandAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CommandAPI.Profiles
 {
